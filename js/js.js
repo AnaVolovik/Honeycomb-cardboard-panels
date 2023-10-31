b0ec0337c9f867a3d1ac4ae7e3d0ac587e9e0894
AnaVolovik <nastasia.volovik@gmail.com>
2023-10-31 17:45:15 +0300

    Add form to project and clean code

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-//SLIDER
+// SLIDER
 
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slider__item');
@@ -15,5 +15,68 @@ setInterval(() => {
   slides[activeSlide].classList.add('active');
 }, 8000);
 
-//end of slider
+// end of slider
 
+// FORM
+
+const orderButton = document.querySelector('#js-go-to-form');
+const orderForm = document.querySelector('.form');
+const closeIcon = document.querySelector('.form__icon');
+const wrapperBgc = document.querySelector('#js-wrapper-shaded');
+
+orderButton.addEventListener('click', () => {
+  orderForm.classList.add('_opened');
+  wrapperBgc.classList.add('_shaded');
+})
+
+closeIcon.addEventListener('click', () => {
+  orderForm.classList.remove('_opened');
+  wrapperBgc.classList.remove('_shaded');
+})
+
+const inputs = document.querySelectorAll('.form__input');
+let name = document.querySelector('#js-input-name');
+let phone = document.querySelector('#js-input-phone');
+
+function validateForm(form) {
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].classList.remove('required');
+  }
+
+  const nameValue = isNotEmpty(form.name);
+  const phoneValue = isNotEmpty(form.phone);
+
+  const correctName = isCorrect(form.name, /^[аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩъЪыЫьЬэЭюЮяЯ]+$/);
+  const correctPhone = isCorrect(form.phone, /^\s*\+?375((33\d{7})|(29\d{7})|(44\d{7}|)|(25\d{7}))\s*$/);
+
+  return nameValue && phoneValue && correctName && correctPhone;
+}
+
+function isNotEmpty(input) {
+  for (let i = 0; i < inputs.length; i++) {
+    input = inputs[i];
+    if (input.value.trim() === '') {
+      input.required = true;
+      input.classList.add('required');
+      return false;
+    }
+  }
+  return true;
+}
+
+function isCorrect(val, regex) {
+  if (!isNotEmpty(val)){
+    return false;
+  }
+  
+  if (!regex.test(val.value)){
+    val.required = true;
+    val.classList.add('required');
+    return false;
+  }
+
+  wrapperBgc.classList.remove('_shaded');
+  return true;
+}
+
+// end of form
